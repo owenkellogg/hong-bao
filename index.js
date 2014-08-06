58584dee3632fff42c9e9bd0183a745d288e73a2
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-06 02:11:34 -0700

    [FEATURE] Add select amount to buy prompt.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var router = new express.Router();
 router.use('/', express.static(__dirname+'/public'));
-router.use('/buy-xrp', function(request, response) {
+/*router.use('/buy-xrp', function(request, response) {
   response.render('buy-xrp');
 });
+*/
 
 module.exports =  router;
 
