c2c6d05367fe9cd86043f05a89289ed9164dee95
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-08 01:04:48 -0700

    [FEATURE] Show loading screen soon.

--- public/js/main.js
+++ public/js/main.js
@@ -121,45 +121,45 @@ $(function() {
           sourceAddress: token.id,
           sourceAmount: dollars
         });
+        $('.submittingTransaction').removeClass('hidden');
+        $('.nameForm').addClass('hidden');
+        $('.prices').addClass('hidden');
+        $('.transactionConfirmed').addClass('hidden');
+        var webSocket = new WebSocket('wss://s1.ripple.com');
+        webSocket.onopen = function() {
+          webSocket.send(JSON.stringify({
+            "command": "subscribe",
+            "accounts": [rippler.get('address')]
+          }));
+        };
+        webSocket.onmessage = function(event) {
+          try {
+            var message = JSON.parse(event.data);
+            var transaction = message.transaction;
+            if (transaction.Destination === rippler.get('address')){
+              if (transaction.Account === 'rUy57HSCpdbDDhzB58Y2NjaomzuqCxaxd3') {
+                console.log(transaction);
+                $('.submittingTransaction').addClass('hidden');
+                $('.nameForm').addClass('hidden');
+                $('.prices').addClass('hidden');
+                $('.transactionConfirmed').removeClass('hidden');
+                $('#transactionAmount').html(''+(transaction.Amount / 1000000)+' XRP');
+                $('#transactionHash').html(transaction.hash);
+                $('#viewInGraph').attr('href', 'https://ripple.com/graph#'+transaction.hash);
+              } else {
+                console.log('transaction to address but not from bridge.');
+              }
+            } else {
+              console.log('transaction involving but not to address.');
+            }
+          } catch(exception) {
+            console.log('error', exception);
+          }
+        };
         inboundBridgeClient.postGatewayPayment(payment, function(error, response) {
           if (error) {
-            alert('error');
-          } else {
-            $('.submittingTransaction').removeClass('hidden');
-            $('.nameForm').addClass('hidden');
-            $('.prices').addClass('hidden');
-            $('.transactionConfirmed').addClass('hidden');
-            var webSocket = new WebSocket('wss://s1.ripple.com');
-            webSocket.onopen = function() {
-              webSocket.send(JSON.stringify({
-                "command": "subscribe",
-                "accounts": [rippler.get('address')]
-              }));
-            };
-            webSocket.onmessage = function(event) {
-              try {
-                var message = JSON.parse(event.data);
-                var transaction = message.transaction;
-                if (transaction.Destination === rippler.get('address')){
-                  if (transaction.Account === 'rUy57HSCpdbDDhzB58Y2NjaomzuqCxaxd3') {
-                    console.log(transaction);
-                    $('.submittingTransaction').addClass('hidden');
-                    $('.nameForm').addClass('hidden');
-                    $('.prices').addClass('hidden');
-                    $('.transactionConfirmed').removeClass('hidden');
-                    $('#transactionAmount').html(''+(transaction.Amount / 1000000)+' XRP');
-                    $('#transactionHash').html(transaction.hash);
-                    $('#viewInGraph').attr('href', 'https://ripple.com/graph#'+transaction.hash);
-                  } else {
-                    console.log('transaction to address but not from bridge.');
-                  }
-                } else {
-                  console.log('transaction involving but not to address.');
-                }
-              } catch(exception) {
-                console.log('error', exception);
-              }
-            };
+            alert('ERROR! Your credit card was not charged.');
+            router.navigate('/', { trigger: true });
           }
         });
       }
