74d32dd3294a621eea574efd01a6c9ec17c1a143
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-21 15:28:00 -0700

    [FEATURE] Improve OpenExchangeClient and CurrencyConverter.

--- test/open_exchange_client.js
+++ test/open_exchange_client.js
@@ -1,18 +1,25 @@
 var Exchange = require(__dirname+'/../lib/open_exchange_client');
+var assert = require('assert');
 
 describe('Exchange Rate', function(){
-  before(function(done){
+  before(function(){
     exchange = new Exchange();
-    done();
   });
 
   it('should list all results', function(done){
-    exchange.getRates(function(error, rates){
-      console.log('rates', rates);
+    exchange.getRates('USD',function(error, rates){
       done();
     });
-    
+  });
 
+  it('should find the rate between two currencies', function(done) {
+    exchange.convert({
+      from_currency: 'USD',
+      to_currency: 'CNY'
+    }, function(error, rate) {
+      console.log('RATE', rate);
+      assert(rate > 2);
+    });
   });
 
 });
